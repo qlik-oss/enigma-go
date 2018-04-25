149aebe6bba483d7f1e28cd38b233c935479c7a5
Mattias Wennmo <wennmo@users.noreply.github.com>
2018-04-25 15:28:53 +0200

    Run examples as integration test in Circle CI (#6)
    * Use running examples as integration tests
    
    * Copy full go path to container

---
+++ examples/run_examples.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+set -e
+
+examples=(
+    basics/custom-type/custom-type.go
+    basics/lists/app-object-list/app-object-list.go
+    basics/lists/field-list/field-list.go
+    basics/events/events.go
+    data/hypercubes/pivot/hypercube-pivot.go
+    data/hypercubes/stacked/hypercube-stacked.go
+    data/hypercubes/straight/hypercube-straight.go
+    data/list-object/list-object.go
+    data/string-expression/string-expression.go
+    interceptors/retry-aborted/retry-aborted.go
+    interceptors/metrics/metrics.go
+    reload/monitor-progress/monitor-progress.go
+    testing/mock_mode/mock_mode.go
+)
+
+for example in ${examples[@]}; do
+	echo
+	echo "Starting example: $example"
+        go run $example
+	echo "Ending example: $example"
+done
