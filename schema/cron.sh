fdb9027db710ce05909642647570134e6eb81533
Andrée Hansson <peolanha@gmail.com>
2018-05-04 09:59:15 +0200

    Update cron.sh

--- schema/cron.sh
+++ schema/cron.sh
@@ -29,7 +29,7 @@ if [ ! -z "$existing_branch" ]; then
   exit 1
 fi
 
-# Generate enigma-go based on latest published Qlik Analytics Engine image
+# Generate enigma-go based on latest published Qlik Associative Engine image
 ./schema/generate.sh
 
 # If there are changes to qix_generated.go then open a pull request
@@ -42,7 +42,7 @@ if [ ! -z "$local_changes" ]; then
   git push -u origin $branch_name
   curl -u none:$GH_TOKEN https://api.github.com/repos/qlik-oss/enigma-go/pulls --request POST --data "{
         \"title\": \"Automated: Generated enigma-go based on new JSON-RPC API\",
-        \"body\": \"Hello! This is an automated pull request.\n\nI have generated a new enigma-go based on the JSON-RPC API for Qlik Analytics Engine.\",
+        \"body\": \"Hello! This is an automated pull request.\n\nI have generated a new enigma-go based on the JSON-RPC API for Qlik Associative Engine.\",
         \"head\": \"$branch_name\",
         \"base\": \"master\"
       }"
