318276acfd55ef59203a1f89a727a2b2208681ec
Alexander Pålsson <fdg@qlik.com>
2019-08-14 14:18:59 +0200

    Made it more clear

--- examples/cookiejar/cookietest.go
+++ examples/cookiejar/cookietest.go
@@ -16,27 +16,16 @@ func main() {
 
 	// Set up the dialer and Header
 	dialer := enigma.Dialer{}
-	header := http.Header{}
 
 	//Create cookiejar
-	jar, err := cookiejar.New(nil)
+	jar, err := dummyCookieJar()
 	if err != nil {
 		fmt.Print(err)
 	}
+
 	dialer.Jar = jar
 
-	// Fill Jar with cookies
-	exp := fmt.Sprintf("%v", time.Now().Local().Add(time.Hour*time.Duration(48)).UTC())
-	header.Add("Set-Cookie", "_session=a518840f-893b-4baf-bdf8-10d78ec14bf5; path=/; expires="+exp+"; secure; httponly")
-	header.Add("Set-Cookie", "_grant=1d3cdfb9-25d0-42b2-8274-d4b11b97a475; path=/interaction/1d3cdfb9-25d0-42b2-8274-d4b11b97a475; expires="+exp+"; secure; httponly")
-	header.Add("Set-Cookie", "_grant=1d3cdfb9-25d0-42b2-8274-d4b11b97a475; path=/auth/1d3cdfb9-25d0-42b2-8274-d4b11b97a475; expires="+exp+"; secure; httponly")
-	response := http.Response{Header: header}
-	cookies := response.Cookies()
-
-	// Set the cookies
-	url, err := url.Parse("ws://localhost:9076/app/engineData")
-	dialer.Jar.SetCookies(url, cookies)
-
+	// Create a session with header
 	ctx := context.Background()
 	global, _ := enigma.Dialer{}.Dial(ctx, "ws://localhost:9076/app/engineData", nil)
 
@@ -48,4 +37,24 @@ func main() {
 	global.DisconnectFromServer()
 	fmt.Println(fmt.Sprintf("Connected to engine version %s.", version.ComponentVersion))
 
+}
+
+// Creates a cookiejar fills it with cookies
+func dummyCookieJar() (jar http.CookieJar, err error) {
+
+	// Fill Jar with cookies
+	jar, err = cookiejar.New(nil)
+	header := http.Header{}
+	exp := fmt.Sprintf("%v", time.Now().Local().Add(time.Hour*time.Duration(48)).UTC())
+	header.Add("Set-Cookie", "_session=a518840f-893b-4baf-bdf8-10d78ec14bf5; path=/; expires="+exp+"; secure; httponly")
+	header.Add("Set-Cookie", "_grant=1d3cdfb9-25d0-42b2-8274-d4b11b97a475; path=/interaction/1d3cdfb9-25d0-42b2-8274-d4b11b97a475; expires="+exp+"; secure; httponly")
+	header.Add("Set-Cookie", "_grant=1d3cdfb9-25d0-42b2-8274-d4b11b97a475; path=/auth/1d3cdfb9-25d0-42b2-8274-d4b11b97a475; expires="+exp+"; secure; httponly")
+	response := http.Response{Header: header}
+	cookies := response.Cookies()
+
+	// Set the cookies
+	url, err := url.Parse("ws://localhost:9076/app/engineData")
+	jar.SetCookies(url, cookies)
+
+	return
 }
