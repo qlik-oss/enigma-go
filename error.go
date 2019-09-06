a8eedc65c79a5ef0fcf8c33e52a32d6df9cdadf9
Marcus de Lacerda <dxa@qlik.com>
2019-09-06 10:16:42 +0200

    Compatible with 1.11, added error tests.

--- error.go
+++ error.go
@@ -22,11 +22,7 @@ type (
 )
 
 func (err *qixError) Error() string {
-	errorType := errorCodeLookup(err.ErrorCode)
-	if errorType[:7] == "LOCERR_" {
-		errorType = errorType[7:]
-	}
-	errorType = strings.ReplaceAll(errorType, "_", " ")
+	errorType := err.codeLookup()
 	return fmt.Sprintf("%s: %s (%d %s)", err.ErrorParameter, err.ErrorMessage, err.ErrorCode, errorType)
 }
 
@@ -40,4 +36,19 @@ func (err *qixError) Parameter() string {
 
 func (err *qixError) Message() string {
 	return err.ErrorMessage
+}
+
+func (err *qixError) codeLookup() string {
+	errorType := errorCodeLookup(err.ErrorCode)
+	if len(errorType) < 7 {
+		// All error codes should result in a string prefixed with LOCERR_
+		// if the code is valid, so this is probably invalid.
+		// Just return whatever was returned by the lookup to be safe.
+		return errorType
+	}
+	if errorType[:7] == "LOCERR_" {
+		errorType = errorType[7:]
+	}
+	errorType = strings.Replace(errorType, "_", " ", -1)
+	return errorType
 }
