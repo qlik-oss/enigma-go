572fc04e5cbc14ef75b964108ec9e885a2355ad9
Marcus de Lacerda <dxa@qlik.com>
2020-03-31 10:56:20 +0200

    Add more json-marshalling tests

--- float_test.go
+++ float_test.go
@@ -7,7 +7,75 @@ import (
 	"testing"
 )
 
-func TestFloat_UnmarshalJSON(t *testing.T) {
+func TestFloatMarshalJSON(t *testing.T) {
+	cases := []struct{
+		float float64
+		exp string
+	}{
+		{math.NaN(), `"NaN"`},
+		{math.Inf(1), `"Infinity"`},
+		{math.Inf(-1), `"-Infinity"`},
+		{1.0, "1"},
+	}
+	for _, cas := range cases {
+		float := Float64(cas.float)
+		ptr := &float
+		var floatBytes, ptrBytes []byte
+		var err error
+		if floatBytes, err = json.Marshal(float); err != nil {
+			t.Error(err)
+			continue
+		}
+		if ptrBytes, err = json.Marshal(ptr); err != nil {
+			t.Error(err)
+			continue
+		}
+		if string(floatBytes) != string(ptrBytes) {
+			t.Errorf("JSON should be equal for pointer and value")
+			t.Log(" Float64:", string(floatBytes))
+			t.Log("*Float64:", string(ptrBytes))
+		}
+		if string(floatBytes) != cas.exp {
+			t.Errorf("Expected %q got %q", cas.exp, string(floatBytes))
+		}
+	}
+}
+
+func TestFloatUnmarshalJSON(t *testing.T) {
+	cases := []struct{
+		b string
+		exp Float64
+	}{
+		{`"NaN"`, Float64(math.NaN())},
+		{`"Infinity"`, Float64(math.Inf(1))},
+		{`"-Infinity"`, Float64(math.Inf(-1))},
+		{"1", 1},
+	}
+	for _, cas := range cases {
+		var f Float64
+		err := json.Unmarshal([]byte(cas.b), &f)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if f != cas.exp {
+			if !(math.IsNaN(float64(f)) && math.IsNaN(float64(cas.exp))) {
+				t.Errorf("Expected '%T: %v' got '%T: %v'",
+					cas.exp, cas.exp, f, f)
+			}
+		}
+	}
+}
+
+func TestFloatNonPointerUnmarshalJSON(t *testing.T) {
+	var f Float64
+	err := json.Unmarshal([]byte("1"), f)
+	if err == nil {
+		t.Error("Should not be able to unmarshal non-pointer")
+	}
+}
+
+func TestFloatCompoundUnmarshalJSON(t *testing.T) {
 	var cell NxCell
 	_ = json.Unmarshal([]byte(`{"qNum":"Infinity"}`), &cell)
 	assert.True(t, math.IsInf(float64(cell.Num), 1))
@@ -25,7 +93,7 @@ func TestFloat_UnmarshalJSON(t *testing.T) {
 	assert.Equal(t, float64(-125), float64(cell.Num))
 }
 
-func TestFloat_MarshalJSON(t *testing.T) {
+func TestFloatCompoundMarshalJSON(t *testing.T) {
 	var cell NxCell
 	var result []byte
 	cell.Num = -125
