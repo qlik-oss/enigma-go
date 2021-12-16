9d4f363dc88d21db8f41d919583d9138adafa457
Aiham Azmeh <aihazm@yahoo.com>
2021-12-16 10:24:37 +0100

    fix: MSG not found

--- schema/check_spec_generate.sh
+++ schema/check_spec_generate.sh
@@ -4,7 +4,7 @@
 git diff --exit-code origin/master -- ./spec/engine-rpc.json > /dev/null
 if [[ $? -ne 0 ]]; then
   ENGINE_VERSION=$(cat ./spec/engine-rpc.json | jq -r '.info.version')
-  MSG "Generating enigma-go based on OPEN-RPC API for Qlik Associative Engine version $ENGINE_VERSION"
+  MSG="Generating enigma-go based on OPEN-RPC API for Qlik Associative Engine version $ENGINE_VERSION"
   echo $MSG
   go run ./schema/generate.go ./spec/engine-rpc.json ./schema/schema-companion.json ./qix_generated.go enigma disable-enigma-import
   go fmt ./qix_generated.go > /dev/null
