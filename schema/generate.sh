447e51724352c898c0d988ab72645bda7735531b
Marcus Lacerda <dxa@qlik.com>
2024-05-15 16:14:45 +0200

    fix: always generate from latest (#326)
    * generate fetches openrpc from QCS
    
    * generate from latest specs

--- schema/generate.sh
+++ schema/generate.sh
@@ -2,9 +2,18 @@
 
 ## compares engine spec with master
 
-wget -q https://qlik.dev/specs/json-rpc/qix.json -O ./schema/engine-rpc.json
-git diff --exit-code origin/master -- ./schema/engine-rpc.json >/dev/null
-if [[ $? -ne 0 ]]; then
+generate() {
+  if [[ $QCS_HOST == "" ]]; then
+    echo "QCS_HOST not set"
+    return 1
+  fi
+  if [[ $QCS_API_KEY  == "" ]]; then
+    echo "QCS_API_KEY not set"
+    return 1
+  fi
+  curl -s -H "Authorization: Bearer $QCS_API_KEY" https://$QCS_HOST/api/engine/openrpc | jq > ./schema/engine-rpc.json
+  git diff --exit-code origin/master -- ./schema/engine-rpc.json >/dev/null
+  if [[ $? -ne 0 ]]; then
     ENGINE_VERSION=$(cat ./schema/engine-rpc.json | jq -r '.info.version')
     echo "Generating enigma-go based on OPEN-RPC API for Qlik Associative Engine version $ENGINE_VERSION"
     ## generate code
@@ -13,6 +22,9 @@ if [[ $? -ne 0 ]]; then
     go fmt ./qix_generated.go >/dev/null
     ## generate spec
     go run ./spec/generate.go
-else
+  else
     echo "No changes to engine-rpc.json, nothing to do."
-fi
+  fi
+}
+
+generate
